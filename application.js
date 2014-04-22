642ee8206b2d763f253872704818312cf9b6b6ab
Nicholas Valbusa <squallstar@gmail.com>
2014-04-22 21:57:31 +0100

    app boilerplate

---
+++ application.js
@@ -0,0 +1,13 @@
+App = new Backbone.Marionette.Application();
+
+App.rootRoute = 'welcome';
+
+App.on("initialize:before", function(options){
+  // do something (if needed, ofc)
+});
+
+App.on("initialize:after", function(options){
+  if (Backbone.history){
+    Backbone.history.start();
+  }
+});
