bf64db8e628438d80d030ee06f6b3bb60cf57dd3
Nicholas Valbusa <squallstar@gmail.com>
2014-05-05 14:09:42 +0100

    collection/model that works

--- javascript/apps/blog/posts/controller.js
+++ javascript/apps/blog/posts/controller.js
@@ -2,7 +2,7 @@ App.module("BlogApp.Posts", function(Posts, App, B, M, $, _){
 
   Posts.Controller = {
     Show: function(){
-      var layout = new Posts.View(new App.Entities.Posts);
+      var layout = new Posts.View({collection: new App.Entities.Posts});
       App.mainRegion.show(layout);
     }
   };
--- javascript/apps/blog/posts/view.js
+++ javascript/apps/blog/posts/view.js
@@ -1,8 +1,28 @@
 App.module("BlogApp.Posts", function(Posts, App, Backbone, M, $, _){
 
-  Posts.View = Backbone.Marionette.View.extend({
+  Posts.PostView = Backbone.Marionette.ItemView.extend({
+    tagName: "li",
+    className: "post",
+    template: "post-template",
+    events: {
+      "click a" : "showSinglePost"
+    },
+    showSinglePost: function(event){
+      event.preventDefault();
+      Backbone.history.navigate("posts/" + this.model.get('id'));
+    }
+  });
+
+  Posts.View = Backbone.Marionette.CompositeView.extend({
     tagName: "section",
-    className: "posts"
+    className: "posts",
+    template: "posts-template",
+    itemView: Posts.PostView,
+    itemViewContainer: "ul",
+
+    initialize: function(options){
+      options.collection.fetch();
+    }
   });
 
 });
